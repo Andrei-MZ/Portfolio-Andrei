c5828adc4ccfc977e3ccdb6bf4696f9107d14080
Andrei Zebrowski <156377022+Andrei-MZ@users.noreply.github.com>
2024-05-24 11:56:11 -0300

    novo header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 const links = ["Github", "Linkedin", "Contato"];
+
 export default function Header() {
   return (
-    <nav className="bg-gray-800 flex justify-between items-center h-15 p-4">
+    <nav className="flex items-center py-8 xl:py-12 text-white bg-pink-50/20">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -16,7 +19,11 @@ export default function Header() {
           d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
         />
       </svg>
-      <p className="text-yellow-50 ml-4 font-semibold text-1xl mr-auto">Andrei Zebrowski</p>
+      <Link href="/">
+      <h1 className="text-3xl font-semibold">Andrei MZ
+      <span className="text-accent">.</span>
+      </h1>
+      </Link>
       <ul className="flex gap-6 list-none text-gray-200 font-semibold">
         {links.map((link)=><li key={link}><a href="#" className=" p-1 hover:bg-gray-700 hover:rounded-md active:bg-purple-700">{ link }</a></li>)}
       </ul>
