96fb90c67df552b6d43bc8a8755b74b5e262db0e
Andrei Zebrowski <156377022+Andrei-MZ@users.noreply.github.com>
2024-05-31 20:53:59 -0300

    new

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -23,20 +23,22 @@ const links = [
 
 const Nav = () => {
     const pathname = usePathname();
-  return (
-  <nav className="flex gap-8">
-    {links.map((link, index)=>{
-        return (
-        <Link href={link.path} key={index} 
-        className={
-            `${link.path === pathname && "text-accent border-b-2 border-accent"   
-        } capitalize font-medium hover:text-accent transition-all`}>
-            {link.name}
-        </Link>
-        );
-    })}
-  </nav>
-  );
+    return (
+        <nav className="flex gap-8">
+            {links.map((link, index) => {
+                const isActive = link.path === pathname;
+                return (
+                    <Link 
+                        href={link.path} 
+                        key={index} 
+                        className={`capitalize font-medium hover:text-accent transition-all ${isActive ? 'text-accent border-b-2 border-accent' : ''}`}
+                    >
+                        {link.name}
+                    </Link>
+                );
+            })}
+        </nav>
+    );
 };
 
 export default Nav;
