0e737ad6da0f3a1626b1a2cc24f6a2905da1abc4
Andrei Zebrowski <156377022+Andrei-MZ@users.noreply.github.com>
2024-05-31 20:53:35 -0300

    mobile

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -24,18 +24,36 @@ const links = [
     },
 ]
 
-
 const MobileNav = () => {
     const pathname = usePathname();
-  return <Sheet>
-    <SheetTrigger className="flex justify-center items-center">
-        <CiMenuFries className="text-[32px] text-accent" />
-    </SheetTrigger>
-    <SheetContent className="flex flex-col">
-        {/* logo */}
-        <div>logo</div>
-    </SheetContent>
-  </Sheet>
+    return (
+        <Sheet>
+            <SheetTrigger className="flex justify-center items-center">
+                <CiMenuFries className="text-[32px] text-accent" />
+            </SheetTrigger>
+            <SheetContent className="flex flex-col">
+                {/* logo */}
+                <div className="mt-32 mb-40 text-center text-2xl">
+                    <Link href="/">
+                        <h1 className='text-4xl font-semibold'>
+                            Andrei MZ<span className='text-accent'>.</span></h1>
+                    </Link>
+                </div>
+                {/* nav */}
+                <nav className='flex flex-col justify-center items-center gap-8'>
+                    {links.map((link, index) => {
+                        const isActive = link.path === pathname;
+                        return (
+                            <Link href={link.path} key={index} className={`text-xl capitalize hover:text-accent transition-all ${isActive ? "text-accent border-b-2 border-accent" : ""}`}
+                            >
+                                {link.name}
+                            </Link>
+                        );
+                    })}
+                </nav>
+            </SheetContent>
+        </Sheet>
+    );
 };
 
-export default MobileNav
+export default MobileNav;
