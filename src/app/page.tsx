e102e53b58303baa0ba33dc6198a6372c42b63f4
Andrei Zebrowski <156377022+Andrei-MZ@users.noreply.github.com>
2024-05-31 21:49:11 -0300

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
   return (
-    <main>
-      <h1>Home page</h1>
-    </main>  
-    
+  <section className="h-full">
+    <div className="container mx-auto h-full">
+      <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+        {/* text */}
+        <div className="text-center xl:text-left">
+          <span className="text-xl">Front-end Developer</span>
+          <h1 className="h1 mb-6">
+            Olá sou <br/>
+            <span className="text-accent">Andrei Zebrowski</span>
+            </h1>
+          <p className="max-w-[500px] mb-9 text-white/80">Sou estudando e desenvolvedor Front-end apaixonado por tecnologia e com uma insaciável vontade de aprender.</p>
+        </div>
+        {/* photo */}
+        <div>photo</div>
+      </div>
+    </div>
+  </section>
   );
-}
+};
+
+export default Home
