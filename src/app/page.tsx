948aa676a535e3ae24d4957cd067622799191e57
Andrei Zebrowski <156377022+Andrei-MZ@users.noreply.github.com>
2024-06-02 20:45:17 -0300

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
-import { FiDownload } from "react-icons/fi";
+import { BiFileFind } from "react-icons/bi";
+import Social from './../components/Social';
+import Photo from "@/components/Photo";
 
 const Home = () => {
   return (
@@ -7,16 +9,28 @@ const Home = () => {
     <div className="container mx-auto h-full">
       <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
         {/* text */}
-        <div className="text-center xl:text-left">
+        <div className="text-center xl:text-left order-2 xl:order-none">
           <span className="text-xl">Front-end Developer</span>
           <h1 className="h1 mb-6">
             Olá sou <br/>
             <span className="text-accent">Andrei Zebrowski</span>
             </h1>
-          <p className="max-w-[500px] mb-9 text-white/80">Sou estudando e desenvolvedor Front-end apaixonado por tecnologia e com uma insaciável vontade de aprender.</p>
+          <p className="max-w-[500px] mb-9 text-white/80">Sou estudando e desenvolvedor Front-end apaixonado por tecnologia e com uma insaciável vontade de aprender.
+          </p>
+          <div className="flex flex-col xl:flex-row items-center gap-8">
+            <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+              <a href="https://docs.google.com/document/d/1V9h_saYlHNEOJk-Ob3RB29aChe6oyn_43V26QkOCGXo/edit?usp=sharing" target="_blank ">Visualizar CV</a>
+              <BiFileFind className="text-xl" />
+            </Button>
+            <div className="mb-8 xl:mb-0">
+              <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
+              </div>
+          </div>
         </div>
         {/* photo */}
-        <div>photo</div>
+        <div className="order-1 xl:order-none mb-8 xl:mb-0">
+          <Photo />
+        </div>
       </div>
     </div>
   </section>
