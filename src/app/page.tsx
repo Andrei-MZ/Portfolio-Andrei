3389b5b867f4239e1b08bac2787175774d2d2149
Andrei Zebrowski <156377022+Andrei-MZ@users.noreply.github.com>
2024-06-02 21:26:18 -0300

    page home quase pronta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { BiFileFind } from "react-icons/bi";
 import Social from './../components/Social';
 import Photo from "@/components/Photo";
+import Stats from './../components/Stats';
 
 const Home = () => {
   return (
@@ -33,6 +34,7 @@ const Home = () => {
         </div>
       </div>
     </div>
+    <Stats />
   </section>
   );
 };
