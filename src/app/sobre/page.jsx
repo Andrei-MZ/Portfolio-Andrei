28d7cb871b79ea2dbe29994d0763221257320691
Andrei Zebrowski <156377022+Andrei-MZ@users.noreply.github.com>
2024-06-09 16:19:07 -0300

    new text-h3/education

--- src/app/sobre/page.jsx
+++ src/app/sobre/page.jsx
@@ -189,7 +189,7 @@ const Sobre = () => {
             </div>
           </TabsContent>
           {/* educação */}
-          <TabsContent value="education" className="w-full">
+          <TabsContent value="education" className="w-full h-full">
           <div className="flex flex-col gap-[30px] text-center xl:text-left">
               <h3 className="text-4xl font-bold">{education.title}</h3>
               <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{education.description}
@@ -202,11 +202,15 @@ const Sobre = () => {
                       className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
                       >
                           <span className="text-accent">{item.duration}</span>
-                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
+                          <h3 className="text-balance max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.position}
+                            </h3>
                           <div className="flex items-center gap-3">
                             {/* dot */}
                             <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
-                            <p className="text-white/60">{item.institution}</p>
+                            <p className="text-white/60">
+                            {item.institution}
+                            </p>
                           </div>
                       </li>
                     )
