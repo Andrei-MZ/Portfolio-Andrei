c931287fa8add6eb2ae8550bff4bc0ca5caeb66c
Andrei Zebrowski <156377022+Andrei-MZ@users.noreply.github.com>
2024-06-11 19:37:01 -0300

    work

--- src/app/trabalho/page.jsx
+++ src/app/trabalho/page.jsx
@@ -1,3 +1,31 @@
+"use client";
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
+
+const projects = [
+  {
+    num: "01",
+    category: "frontend",
+    title: "projeto 1",
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus, ratione.",
+    stack: [{ name: "Javascript"}, { name: "Tailwind"}, { name: "Next.js"}],
+    
+  },
+]
+
+
+
 const Trabalho = () => {
   return <div>trabalho</div>;
 };
