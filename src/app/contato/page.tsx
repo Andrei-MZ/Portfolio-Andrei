eb3121f305051a3e7d7d2b29a2b0f148e6974684
Andrei Zebrowski <156377022+Andrei-MZ@users.noreply.github.com>
2024-06-16 17:01:48 -0300

    finish

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -20,17 +20,17 @@ const info = [
   {
     icon: <FaPhoneAlt />,
     title: "Telefone",
-    Description: "(+051) 9 9917-2556",
+    description: "(+051) 9 9917-2556",
   },
   {
     icon: <FaEnvelope />,
     title: "Email",
-    Description: "andreimendes8@gmail.com",
+    description: "andreimendes8@gmail.com",
   },
   {
     icon: <FaMapMarkerAlt />,
     title: "Endereço",
-    Description: "Santa Cruz do Sul, RS",
+    description: "Santa Cruz do Sul, RS",
   },
 ];
 
@@ -46,7 +46,7 @@ const Contato = () => {
   <div className="container mx-auto">
     <div className="flex flex-col xl:flex-row gap-[30px]">
       {/* forma */}
-      <div className="xl:h-[54px] order-2 xl:order-none">
+      <div className="xl:w-[54%] order-2 xl:order-none">
         <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
           <h3 className="text-4xl text-accent">Vamos trabalhar juntos</h3>
           <p className="text-white/60">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam atque sit, accusamus excepturi similique suscipit aut animi ipsa soluta placeat ipsam blanditiis. Quam provident doloribus perspiciatis illo, deserunt sequi voluptatum?
@@ -58,24 +58,35 @@ const Contato = () => {
             <Input type="email" placeholder="Email"/>
             <Input type="phone" placeholder="Numero Telefone"/>
           </div>
-          {/* select */}
-          <Select>
-            <SelectTrigger className="w-full">
-              <SelectValue placeholder="Select a service" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>Select a service</SelectLabel>
-                  <SelectItem value="est">Web Development</SelectItem>
-                  <SelectItem value="est">Web Development</SelectItem>
-                  <SelectItem value="est">Web Development</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-          </Select>
+          {/* textarea */}
+            <Textarea
+            className="h-[200px]"
+            placeholder="Type your message here"
+            />
+            {/* btn */}
+            <Button size="md" className="max-w-40">
+              Send message
+              </Button>
           </form>
       </div>
       {/* info */}
-      <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">info</div>
+      <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+        <ul className="flex flex-col gap-10">
+          {info.map((item, index)=> {
+            return (
+              <li key={index} className="flex items-center gap-6">
+                <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                  <div className="text-[28px]">{item.icon}</div>
+                </div>
+                <div className="flex-1">
+                  <p className="text-white/60">{item.title}</p>
+                  <h3 className="text-xl">{item.description}</h3>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+        </div>
     </div>
   </div>
   </motion.section>;
