36d4e41be4019171f328a8a5b6f49d9c47ebfda9
Andrei Zebrowski <156377022+Andrei-MZ@users.noreply.github.com>
2024-06-16 19:17:20 -0300

    contato

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -4,17 +4,8 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 
-import { 
-  Select, 
-  SelectContent, 
-  SelectGroup, 
-  SelectItem, 
-  SelectLabel, 
-  SelectTrigger, 
-  SelectValue, } from "@radix-ui/react-select";
 
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
-import { Description } from "@radix-ui/react-dialog";
 
 const info = [
   {
@@ -48,8 +39,8 @@ const Contato = () => {
       {/* forma */}
       <div className="xl:w-[54%] order-2 xl:order-none">
         <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-          <h3 className="text-4xl text-accent">Vamos trabalhar juntos</h3>
-          <p className="text-white/60">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam atque sit, accusamus excepturi similique suscipit aut animi ipsa soluta placeat ipsam blanditiis. Quam provident doloribus perspiciatis illo, deserunt sequi voluptatum?
+          <h3 className="text-4xl text-accent">Mande-me uma mensagem</h3>
+          <p className="text-white/60">Caso gostou de algo me mannde uma mensagem.
           </p>
           {/* Input */}
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
@@ -61,11 +52,11 @@ const Contato = () => {
           {/* textarea */}
             <Textarea
             className="h-[200px]"
-            placeholder="Type your message here"
+            placeholder="Digite sua mensagem aqui"
             />
             {/* btn */}
             <Button size="md" className="max-w-40">
-              Send message
+              Enviar
               </Button>
           </form>
       </div>
